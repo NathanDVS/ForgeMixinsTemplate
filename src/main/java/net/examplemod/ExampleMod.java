66b8fbb83e8f3974cd5e8147c4fa533c8562b025
NathanDV <76598451+NathanDVS@users.noreply.github.com>
2022-04-11 07:44:22 -0400

    Create ExampleMod.java

---
+++ src/main/java/net/examplemod/ExampleMod.java
@@ -0,0 +1,12 @@
+package net.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Tweaker.MODID, name = Tweaker.NAME, version = Tweaker.VERSION)
+public class ExampleMod {
+
+    static final String MODID = "examplemod";
+    static final String NAME = "Example";
+    public static final String VERSION = "1.0.0";
+
+}
